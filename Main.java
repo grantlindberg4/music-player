caefab4f6be17083bad6b5826c9b48f688d418b0
Grant Lindberg <grant.lindberg@yahoo.com>
2016-11-01 20:37:48 -0700

    Remove unused loop in main

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         SoundPlayer soundPlayer = new SoundPlayer();
+
 	    File song = new File("sounds/applause.wav");
-        while(soundPlayer.repeat) {
-            soundPlayer.play(song);
-        }
+
+        soundPlayer.play(song);
     }
 }
